44948cd5600c39fd0956f3345256502b12b22cab
Per Arneng <per.arneng@scalebit.com>
2015-04-06 18:03:35 +0200

    removed generated

--- web/ts/StatusField.js
+++
@@ -1,7 +0,0 @@
-var StatusField = (function () {
-    function StatusField() {
-        this.indicators = [];
-        this.indicators.push(new StatusIndicator());
-    }
-    return StatusField;
-})();
